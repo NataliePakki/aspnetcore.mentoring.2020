2c02ac8734b34540a48f73ed007ca1769fa2215c
Natalie Pakki <natalie.pakki@gmail.com>
2020-03-28 17:26:11 +0300

    [Controllers, Applications, Requests] Task 1 - add unit tests for controllers

---
+++ Shop.Web.Tests/Controllers/CategoriesControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Xunit;
+
+using Shop.Core.Models;
+using Shop.Core.Services;
+using Shop.Web.Controllers;
+using Shop.Web.Helpers;
+using Shop.Web.ViewModels;
+
+namespace Shop.Web.Tests.Controllers
+{
+    public class CategoriesControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsAViewResult_WithAListOfCategories()
+        {
+            // Arrange
+            var categories = GetCategories();
+            var viewModels = categories.ToViewModels();
+            var mockService = new Mock<ICategoryService>();
+            mockService.Setup(s => s.GetAll()).Returns(categories);
+
+            var controller = new CategoriesController(mockService.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<CategoryViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(1, model.Count());
+        }
+
+        private IList<Category> GetCategories()
+        {
+            return new List<Category>()
+            {
+                new Category() {CategoryID = 1 }
+            };
+        }
+    }
+}
---
+++ Shop.Web.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using System.Collections.Generic;
+using Moq;
+using Xunit;
+
+using Shop.Core.Models;
+using Shop.Core.Services;
+using Shop.Web.Controllers;
+using Shop.Web.Helpers;
+using Shop.Web.ViewModels;
+using Shop.Web.Models;
+
+namespace Shop.Web.Tests.Controllers
+{
+    public class ProductsControllerTests
+    {
+        private Mock<IProductService> mockProductsService;
+        private Mock<ICategoryService> mockCategoryService;
+        private Mock<ISupplierService> mockSupplierService;
+
+        private ProductsController controller;
+
+        public ProductsControllerTests()
+        {
+            mockProductsService = new Mock<IProductService>();
+            mockCategoryService = new Mock<ICategoryService>();
+            mockSupplierService = new Mock<ISupplierService>();
+
+            IOptions<Settings> someOptions = Options.Create<Settings>(new Settings() { MaxCountProducts = 0});
+
+            controller = new ProductsController(mockProductsService.Object, mockCategoryService.Object, mockSupplierService.Object, someOptions); ;
+        }
+
+        [Fact]
+        public void Index_ReturnsAViewResult_WithAListOfProducts()
+        {
+            // Arrange
+            var products = GetProducts();
+            var viewModels = products.ToViewModels();
+
+            mockProductsService.Setup(s => s.GetAll()).Returns(products);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<ProductViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(1, model.Count());
+        }
+
+        [Fact]
+        public void Create_ReturnsAViewResult_WithCreateProductView()
+        { 
+            // Act
+            var result = controller.Create();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<CreateProductViewModel>(
+                viewResult.ViewData.Model);
+        }
+
+        [Fact]
+        public void Create_RedirectToIndex()
+        {
+            // Arrange
+            var newProduct = new CreateProductViewModel();
+
+            // Act
+            RedirectToActionResult redirectResult =
+                   (RedirectToActionResult) controller.Create(newProduct);
+
+            // Assert
+            Assert.Equal(redirectResult.ActionName, "Index");
+        }
+
+        [Fact]
+        public void Create_CreateProductCalled()
+        {
+            // Arrange
+            var newProduct = new CreateProductViewModel();
+
+            // Act
+            controller.Create(newProduct);
+
+            // Assert
+            mockProductsService.Verify(x => x.Create(It.IsAny<Product>()));
+        }
+
+        [Fact]
+        public void Edit_RedirectToHome_WhenProductNotExist()
+        {
+            // Arrange
+            var id = 1;
+            mockProductsService.Setup(x => x.Get(id)).Returns<Product>(null);
+
+            // Act
+            RedirectToActionResult redirectResult =
+                   (RedirectToActionResult)controller.Edit(id);
+
+            // Assert
+            Assert.Equal(redirectResult.ActionName, "Index");
+        }
+
+        [Fact]
+        public void Edit_EditProductCalled()
+        {
+            // Arrange
+            var editProductVuewModel = new EditProductViewModel();
+
+            // Act
+            RedirectToActionResult redirectResult =
+                   (RedirectToActionResult)controller.Edit(editProductVuewModel);
+
+            // Assert
+            mockProductsService.Verify(x => x.Update(It.IsAny<Product>()));
+            Assert.Equal(redirectResult.ActionName, "Index");
+        }
+
+        private IList<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product() { ProductID = 1 }
+            };
+        }
+    }
+}
