3c0b21b6d7d82273f47a5987f23e2055a5dfa8f8
Natalie Pakki <natalie.pakki@gmail.com>
2020-04-11 18:11:48 +0300

    [Razor and Viewes] Task 2 - ViewComponents / PartialViews

---
+++ Shop.Web/Components/Breadcrumb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Web.Components
+{
+    [ViewComponent]
+    public class Breadcrumb: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()     
+        {
+            var controllerName = ViewContext.RouteData.Values["controller"].ToString();
+            var actionName = ViewContext.RouteData.Values["action"].ToString();
+            return View(new Tuple<string, string>(controllerName, actionName));
+        }
+    }
+}
