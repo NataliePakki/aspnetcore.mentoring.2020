3b3681195a3485c6d65167076a78d47daa23b06f
Natalie Pakki <natalie.pakki@gmail.com>
2020-04-12 14:35:41 +0300

    [Razor and Viewes] Task 3 - Tag helpers

---
+++ Shop.Web/Helpers/LinkHtmlHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Shop.Web.Helpers
+{
+    public static class LinkHtmlHelper
+    {
+        public static IHtmlContent NorthwindImageLink<TModel, TResult>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TResult>> expression, string imageId, string linkText)
+        {
+            TagBuilder a = new TagBuilder("a");
+            a.InnerHtml.Append(linkText);
+            a.Attributes.Add("href", "/images/" + imageId);
+
+            var writer = new System.IO.StringWriter();
+            a.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
---
+++ Shop.Web/Helpers/LinkTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Shop.Web.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class LinkTagHelper: TagHelper
+    {
+        [HtmlAttributeName("northwind-id")]
+        public int ImageId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", "/images/" + this.ImageId);
+        }
+    }
+}
