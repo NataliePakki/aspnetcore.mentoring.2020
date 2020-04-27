eb62bba8ee047fb8c84f3d97a92c6bb14a2c19e1
Natalie Pakki <natalie.pakki@gmail.com>
2020-04-27 16:47:21 +0300

    [Web API] Task 4 - use Swashbuckle.AspNetCore

--- Shop.API/Startup.cs
+++ Shop.API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Shop.API.Models;
 using Shop.Core.Data;
 using Shop.Core.Services;
@@ -29,6 +30,11 @@ namespace Shop.API
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<ISupplierService, SupplierService>();
             services.AddAutoMapper(typeof(ShopMappingProfile));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shop API", Version = "v1" });
+            });
             services.AddControllers();
 
         }
@@ -47,6 +53,12 @@ namespace Shop.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
